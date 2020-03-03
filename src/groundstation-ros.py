b2b2ebadef5e099ddf19dd41eb63966759e4a921
Sijmen Huizenga <sijmenhuizenga@gmail.com>
2020-03-03 18:26:05 +0100

    Do not auto-detect ros parameters because they are always set because we launch through roslaunch

--- src/groundstation-ros.py
+++ src/groundstation-ros.py
@@ -19,16 +19,4 @@ rosrecorder_ = RosRecorder()
 
 Webserver(rosrecorder_).start()
 
-if os.environ.get('ROS_IP') is None:
-    myip = commands.getoutput("ip addr show | grep 'inet ' | awk '{print $2}' | cut -f1 -d'/' | grep '192.168.1.' | head -n 1")
-    if myip is '':
-        print "No ROS_IP set and could not find current ip in range 192.168.1.*"
-        sys.exit(1)
-    print("Setting ROS_IP to " + myip)
-    os.environ['ROS_IP'] = myip
-
-if os.environ.get('ROS_MASTER_URI') is None:
-    print("ROS_MASTER_URI unset, setting it to http://" + config['host'] + ":11311")
-    os.environ['ROS_MASTER_URI'] = "http://" + config['host'] + ":11311"
-
 RosNode(rosrecorder_).run()
